0063877844a18c5882a6dd4b839ca42896704dd9
manuelgutfuentes <47529399+manuelgutfuentes@users.noreply.github.com>
2019-02-14 22:02:00 +0100

    V1

---
+++ EJ_7.py
@@ -0,0 +1,11 @@
+class Empleado():
+    def init__(self, sueldo, tasa):
+        self.sueldo = sueldo
+        self.tasa = tasa
+    def get_suedo(self):
+        return self.sueldo
+    def get_tasa(self):
+        return self.tasa
+
+emp1=(1200,0.3)
+emp2=1450,0.37)
